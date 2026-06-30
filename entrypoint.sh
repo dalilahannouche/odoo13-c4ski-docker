0c37b33e028a854beb94082875bc1f90dcb882dd
dalilahannouche <dalilahannouche@hotmail.fr>
2026-06-30 20:41:06 +0200

    adding proxy mode on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,11 @@
 set -e
 
 exec odoo \
-    --db_host="$DB_HOST" \
-    --db_port="$DB_PORT" \
-    --db_user="$DB_USER" \
-    --db_password="$DB_PASSWORD" \
-    --database="$DB_NAME" \
-    --config=/etc/odoo/odoo.conf
+  --db_host="$DB_HOST" \
+  --db_port="$DB_PORT" \
+  --db_user="$DB_USER" \
+  --db_password="$DB_PASSWORD" \
+  --db_name="$DB_NAME" \
+  --http-port=$PORT \
+  --proxy-mode \
+  --config=/etc/odoo/odoo.conf
