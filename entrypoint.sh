69cee3b796f81e099e5a3416775808633b5b69c8
dalilahannouche <dalilahannouche@hotmail.fr>
2026-06-30 20:54:11 +0200

    remove DB_NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ exec odoo \
   --db_port="$DB_PORT" \
   --db_user="$DB_USER" \
   --db_password="$DB_PASSWORD" \
-  --db_name="$DB_NAME" \
   --http-port=$PORT \
   --proxy-mode \
   --config=/etc/odoo/odoo.conf
